2233f3336b80d891a3f057690c0fc911003972c1
andreyDanilenko <danilenko.a.g@mail.ru>
2022-09-10 20:23:15 +0300

    fix format date

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import { format } from 'date-fns';
+import cn from 'classnames';
+import styles from './Footer.module.css';
 import { IFooterProps } from './Footer.props';
 
-export const Footer = ({...props}: IFooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: IFooterProps): JSX.Element => {
+
   return (
-    <div {...props}>Footer</div>
-  );
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности</a>
+		</footer>
+	);
 };
